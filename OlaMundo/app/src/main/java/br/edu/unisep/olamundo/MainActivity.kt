ece92bc1e838517b4eafda654b2be368762f5297
rcpadilha <robertocesardasilva@gmail.com>
2019-02-20 18:06:54 -0300

    Projeto Olá Mundo

--- OlaMundo/app/src/main/java/br/edu/unisep/olamundo/MainActivity.kt
+++ OlaMundo/app/src/main/java/br/edu/unisep/olamundo/MainActivity.kt
@@ -2,6 +2,8 @@ package br.edu.unisep.olamundo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun exibirMensagem(v : View) {
+        val t = Toast.makeText(this, "Olá Mundo!!!", Toast.LENGTH_LONG)
+        t.show()
+    }
+
 }
