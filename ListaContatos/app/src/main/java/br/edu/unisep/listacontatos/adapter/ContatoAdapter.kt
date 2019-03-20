7df9d03e0564e0683465840f7ec8418f837b4594
rcpadilha <robertocesardasilva@gmail.com>
2019-03-19 21:47:39 -0300

    Aula 19/03

---
+++ ListaContatos/app/src/main/java/br/edu/unisep/listacontatos/adapter/ContatoAdapter.kt
@@ -0,0 +1,4 @@
+package br.edu.unisep.listacontatos.adapter
+
+class ContatoAdapter {
+}
--- ListaContatos/app/src/main/java/br/edu/unisep/listacontatos/adapter/ContatoViewHolder.kt
+++ ListaContatos/app/src/main/java/br/edu/unisep/listacontatos/adapter/ContatoViewHolder.kt
@@ -1,5 +1,8 @@
 package br.edu.unisep.listacontatos.adapter
 
-class ContatoViewHolder {
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class ContatoViewHolder(v : View) : RecyclerView.ViewHolder(v){
 
 }
