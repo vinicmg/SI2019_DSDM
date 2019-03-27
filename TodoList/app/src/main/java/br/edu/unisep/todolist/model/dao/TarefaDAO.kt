37b390c9eba3b1c3fe9a3d6c5141a04d71e23af3
rcpadilha <robertocesardasilva@gmail.com>
2019-03-27 20:36:16 -0300

    Aula 27/03

--- TodoList/app/src/main/java/br/edu/unisep/todolist/model/dao/TarefaDAO.kt
+++ TodoList/app/src/main/java/br/edu/unisep/todolist/model/dao/TarefaDAO.kt
@@ -1,4 +1,20 @@
 package br.edu.unisep.todolist.model.dao
 
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import br.edu.unisep.todolist.model.entity.Tarefa
+
+@Dao
 interface TarefaDAO {
+
+    @Insert
+    fun salvar(t : Tarefa)
+
+    @Query("select * from Tarefa")
+    fun listar() : List<Tarefa>
+
+    @Delete
+    fun excluir(t : Tarefa)
 }
---
+++ TodoList/app/src/main/java/br/edu/unisep/todolist/model/db/TodoListDB.kt
@@ -0,0 +1,4 @@
+package br.edu.unisep.todolist.model.db
+
+class TodoListDB {
+}
