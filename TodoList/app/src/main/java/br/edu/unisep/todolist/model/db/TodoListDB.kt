2732bb38fa8f540658c51384de42261eb6409e63
rcpadilha <robertocesardasilva@gmail.com>
2019-04-02 22:10:19 -0300

    Aula 02/04

--- TodoList/app/src/main/java/br/edu/unisep/todolist/model/db/TodoListDB.kt
+++ TodoList/app/src/main/java/br/edu/unisep/todolist/model/db/TodoListDB.kt
@@ -1,4 +1,33 @@
 package br.edu.unisep.todolist.model.db
 
-class TodoListDB {
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import br.edu.unisep.todolist.model.dao.TarefaDAO
+import br.edu.unisep.todolist.model.entity.Tarefa
+
+@Database(
+    entities = arrayOf(Tarefa::class),
+    version = 1
+)
+abstract class TodoListDB : RoomDatabase(){
+
+    abstract fun tarefaDAO() : TarefaDAO
+
+    companion object {
+
+        private var db: TodoListDB? = null
+
+        fun getDb(applicationContext: Context): TodoListDB {
+
+            if (db == null) {
+                db = Room.databaseBuilder(applicationContext, TodoListDB::class.java,
+                    "db_todolist").allowMainThreadQueries().build()
+            }
+
+            return db!!
+        }
+    }
+
 }
