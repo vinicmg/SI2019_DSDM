804ef01e272c980ddd831cb3a55af9d744ad7adf
rcpadilha <robertocesardasilva@gmail.com>
2019-04-02 22:44:39 -0300

    Aula 02/04

--- TodoList/app/src/main/java/br/edu/unisep/todolist/adapter/TarefaAdapter.kt
+++ TodoList/app/src/main/java/br/edu/unisep/todolist/adapter/TarefaAdapter.kt
@@ -1,4 +1,32 @@
 package br.edu.unisep.todolist.adapter
 
-class TarefaAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.edu.unisep.todolist.R
+import br.edu.unisep.todolist.model.entity.Tarefa
+
+class TarefaAdapter(val tarefas : List<Tarefa>)
+    : RecyclerView.Adapter<TarefaAdapter.TarefaViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, pos: Int): TarefaViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val v = inflater.inflate(R.layout.item_tarefa, parent, false)
+
+        val viewHolder = TarefaViewHolder(v)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return tarefas.size
+    }
+
+    override fun onBindViewHolder(holder: TarefaViewHolder, pos: Int) {
+
+    }
+
+
+    class TarefaViewHolder(val v : View) : RecyclerView.ViewHolder(v)
+
 }
